d1f247dd4199ae16dfc3fbf62ff1c7e875fcc066
CodeWithNoor <ma8073641@gmail.com>
2023-11-08 00:52:33 +0500

    stripe payment

--- lib/coverter/Subscription.tsx
+++ lib/coverter/Subscription.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import {
+  DocumentData,
+  FirestoreDataConverter,
+  QueryDocumentSnapshot,
+  SnapshotOptions,
+  collection,
+} from "firebase/firestore";
+import { Subscription } from "../../types/Subscription";
 
-const Subscription = () => {
-  return (
-    <div>Subscription</div>
-  )
-}
+const SubscriptionConverter: FirestoreDataConverter<Subscription> = {
+  toFirestore(subscription: Subscription): DocumentData {
+    return { ...subscription };
+  },
+  fromFirestore(
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions
+  ): Subscription {
+    const data = snapshot.data(options);
 
-export default Subscription
+    const sub: Subscription = {
+      id: snapshot.id,
+    };
+  },
+};
+
+export default SubscriptionConverter;
